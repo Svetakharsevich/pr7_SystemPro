cb818a20a480cc0badf43a4c9be1a3a68db6f430
Svetakharsevich <123375587+Svetakharsevich@users.noreply.github.com>
2024-02-10 14:55:23 +0500

    Add files via upload

---
+++ task1/task1.cpp
@@ -0,0 +1,35 @@
+﻿
+#include <fstream>
+using namespace std;
+#include <iostream>
+#include <stdio.h>
+#include <fstream>
+#include<string>
+int Empty(ifstream& t, int emptylines=0) {
+	string line;
+	if (!getline(t, line)) {
+		return emptylines;
+	}
+	if (line.empty()) {
+		return Empty(t, emptylines + 1);
+	}
+	else {
+		return emptylines;
+	}
+}
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	ifstream file ("text.txt");
+	if (file.is_open()) {
+		int emptiLines = Empty(file);
+		cout << "Количество пустых строк перед текстом "<< emptiLines << endl;
+
+		file.close();
+	}
+	else {
+		cout<<"Не удалось открыть файл"<< endl;
+	}
+	return 0;
+}
+
